46d322fa696af0628ce691930c1a6dd31ffcbb44
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-03-31 21:47:46 +0530

    Added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      Hello world
-    </>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-[80%] h-[80%] border border-[#27272A] rounded-lg'>
+        <Header />
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='p-7 flex items-center justify-between'>
+      <div>
+        <p className='text-2xl font-bold'>Welcome back!</p>
+        <p className='text-[#97979F]'>List of your tasks</p>
+      </div>
+      <div>
+        <img src="https://avatars.githubusercontent.com/u/75907582?v=4" alt="userImage" width={35} className='rounded-full' />
+      </div>
+    </div>
+  )
+}
+
+export default Header;
